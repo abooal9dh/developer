0ab2f60c2c1d0af2ba262517bd7d69c0b22d61aa
aBooal9DH <abooal9dh@gmail.com>
2023-10-06 20:39:59 +0300

    io

---
+++ bash/cmd_1.bat
@@ -0,0 +1,6 @@
+START D:\xampp\xampp-control.exe
+START D:\xampp\htdocs\cmd_2.bat
+cls
+D:\xampp\htdocs\coodxroot
+D:\xampp\htdocs\nofakya18.com
+php artisan ser
---
+++ bash/cmd_2.bat
@@ -0,0 +1,5 @@
+START D:\xampp\xampp-control.exe
+START D:\xampp\htdocs\cmd_2.exe
+cls
+D:\xampp\htdocs\coodxroot
+D:\xampp\htdocs\nofakya18.com
