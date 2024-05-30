b771ebc5b11d41b07d037253b926d035fe69d09b
aBooal9DH <aBooal9DH@gmail.com>
2024-05-30 17:45:43 +0300

    up

---
+++ form.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <title>Support</title>
+</head>
+<body dir="rtl" style="background-color: black;">
+  <div class="col-10 display-content-center" style="color: white; margin-bottom: 50px; margin-left: auto;  margin-right: auto;" ><div class="mb-3">
+    <img style="  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;"  src="skatezz.png"><br>
+  <label for="exampleFormControlInput1" class="form-label">الاسم</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">البريد الإلكتروني</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">رقم الجوال</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">ارسال شكوى</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+  <button type="submit" class="btn btn-primary">ارسال</button>
+</div>
+</body>
+</html>
